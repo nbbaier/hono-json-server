190fcac0acc30cb28511bc7a2703d14ea8820e46
Nico Baier <nico.baier@gmail.com>
2024-02-24 12:52:54 -0600

    feat(grouping.tsx): add initial implementation of a new file 'grouping.tsx' for grouping routes and handling authentication feat(serve.ts): add initial implementation of a new file 'serve.ts' for serving the application and handling routes

---
+++ grouping.tsx
@@ -0,0 +1,59 @@
+/** @jsx jsx */
+import { Hono } from "hono";
+import { showRoutes } from "hono/dev";
+import { FC } from "hono/jsx";
+import { jsxRenderer, useRequestContext } from "hono/jsx-renderer";
+import titleize from "titleize";
+import { jwt } from "hono/jwt";
+
+const app = new Hono();
+
+app.use(
+  "/auth/*",
+  jwt({
+    secret: "it-is-very-secret",
+  })
+);
+
+app.get("/auth/page", (c) => {
+  return c.text("You are authorized");
+});
+
+// app.get(
+//   "/page/*",
+//   jsxRenderer(({ children }) => {
+//     return (
+//       <html>
+//         <body>
+//           <header>Menu</header>
+//           <div>{children}</div>
+//         </body>
+//       </html>
+//     );
+//   })
+// );
+
+// const RequestUrlBadge: FC = () => {
+//   const c = useRequestContext();
+//   return <b>{c.req.url}</b>;
+// };
+
+// app.get("/page/info", (c) => {
+//   return c.render(
+//     <div>
+//       You are accessing: <RequestUrlBadge />
+//     </div>
+//   );
+// });
+
+// app.get("/page/:header", (c) => {
+//   return c.render(<h1>{titleize(c.req.param("header"))}</h1>);
+// });
+
+// app.get("/", (c) => c.text("hi"));
+
+Bun.serve({
+  fetch: app.fetch,
+});
+
+console.log(showRoutes(app));
---
+++ serve.ts
@@ -0,0 +1,31 @@
+import { createServer } from "./index.ts";
+import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+
+const schema = z.object({
+  name: z.string().min(5, { message: "Must be 5 or more characters long" }),
+});
+
+const app = new Hono();
+
+app.get("/", (c) => c.text("hono"));
+
+app.get("/:name", zValidator("param", schema), (c) =>
+  c.text(`Hello ${c.req.param("name")}`)
+);
+
+// app.get(
+//   '/posts/:id',
+//   validator('param', ),
+//   validator('query', )
+//   validator('json', ...),
+//   (c) => {
+//     //...
+//   }
+
+const server = Bun.serve({
+  fetch: app.fetch,
+});
+
+console.log(`Listening on http://localhost:${server.port}`);
